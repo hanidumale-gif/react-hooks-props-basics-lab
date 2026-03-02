3c0b1543a608a61b56771a8c07bc7c383e65da91
hanidumale-gif <hanidumale@gmail.com>
2026-03-02 09:35:00 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
-import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
-
 function App() {
+  const user = {
+    name: "Hani",
+    city: "Garissa",
+    bio: "I love building web applications",
+    color: "blue",
+    links: {
+      github: "https://github.com/hanidumale",
+      linkedin: "https://linkedin.com/in/hanidumale"
+    }
+  };
+
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <nav>
+        <Home 
+          id="home"
+          name={user.name} 
+          city={user.city} 
+          color={user.color} 
+        />
+        <About 
+          id="about"
+          bio={user.bio} 
+          links={user.links} 
+        />
+      </nav>
     </div>
   );
 }
